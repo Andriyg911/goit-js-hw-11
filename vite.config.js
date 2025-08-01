84d2eb33a00837807ffb57473f7788b1c1f27830
Andriy <kykyagoloviy@gmail.com>
2025-08-01 14:01:11 +0300

    feat: опис ваших змін перед ребейзом

--- vite.config.js
+++ vite.config.js
@@ -5,43 +5,31 @@ export default defineConfig(({ command }) => {
   const isDev = command === 'serve';
 
   return {
-    // Inject a global or _global object depending on dev vs. build
     define: {
       [isDev ? 'global' : '_global']: {},
     },
-
     root: 'src',
     base: isDev ? '' : '/goit-js-hw-11/',
-
     server: {
-      fs: {
-        allow: ['..']
-      }
+      fs: { allow: ['..'] }
     },
-
     build: {
       sourcemap: true,
       outDir: '../dist',
       emptyOutDir: true,
       rollupOptions: {
-        // Explicit multi-page input
         input: {
-          index:   resolve(__dirname, 'src/index.html'),
-         
+          index: resolve(__dirname, 'src/index.html'),
         },
         output: {
-          // Vendors in a separate chunk
           manualChunks(id) {
             if (id.includes('node_modules')) return 'vendor';
           },
-
-          // Keep file names predictable
           entryFileNames: '[name].js',
           assetFileNames: 'assets/[name]-[hash][extname]'
         }
       }
     },
-
     plugins: []
   };
 });
