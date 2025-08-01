c1ec9812b23a6673c9937ef01d10f2b13cf9bd1b
Andriy <kykyagoloviy@gmail.com>
2025-08-01 15:00:33 +0300

    fix: аліас axios на UMD-бандл, обходимо CJS-парсер

--- vite.config.js
+++ vite.config.js
@@ -5,31 +5,46 @@ export default defineConfig(({ command }) => {
   const isDev = command === 'serve';
 
   return {
-    define: {
-      [isDev ? 'global' : '_global']: {},
-    },
     root: 'src',
-    base: isDev ? '' : '/goit-js-hw-11/',
-    server: {
-      fs: { allow: ['..'] }
+
+    base: isDev ? '/' : '/goit-js-hw-11/',
+
+    // 1) Аліас “axios” на UMD-бандл у node_modules/axios/dist/axios.js
+    resolve: {
+      alias: {
+        axios: resolve(__dirname, 'node_modules/axios/dist/axios.js'),
+      },
     },
+
+    // 2) Препакуємо axios через esbuild, щоб Rollup не чіпав його CJS-джерела
+    optimizeDeps: {
+      include: ['axios'],
+    },
+
     build: {
-      sourcemap: true,
       outDir: '../dist',
       emptyOutDir: true,
+      sourcemap: true,
+
+      // 3) Дозволяємо змішані ES/CJS модулі у разі інших залежностей
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+
       rollupOptions: {
         input: {
           index: resolve(__dirname, 'src/index.html'),
         },
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) return 'vendor';
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
           },
           entryFileNames: '[name].js',
-          assetFileNames: 'assets/[name]-[hash][extname]'
-        }
-      }
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        },
+      },
     },
-    plugins: []
   };
 });
