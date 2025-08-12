b6f16b560b739219b9e055bd974d45fc7443e6a3
Andriy <kykyagoloviy@gmail.com>
2025-08-12 21:51:47 +0300

    fix: define global as window to resolve lightbox error

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ export default defineConfig({
   root: 'src',
   publicDir: '../public',
   base: '/goit-js-hw-11/',
+  define: {
+    global: 'window',
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true,
